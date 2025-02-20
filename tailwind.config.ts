f8e031d20a5e44a4a5c06490674c46bf5e6296eb
jhillbht <jordan@bluehighlightedtext.com>
2025-02-20 14:32:20 -0600

    Resolved merge conflicts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
 import type { Config } from "tailwindcss";
+import animate from 'tailwindcss-animate'
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
 
 export default {
   darkMode: ["class"],
@@ -77,5 +80,9 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    animate,
+    forms,
+    typography,
+  ],
 } satisfies Config;
